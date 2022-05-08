cc71417e34df8674b8eef8dec89475b3c5d07854
Tanveer Bassi <tanveer13a551@gmail.com>
2022-05-08 18:01:37 +0100

    updated functions for LS with Natasha

--- assets/app.js
+++ assets/app.js
@@ -1,31 +1,33 @@
 // wait until Jquery is ready
-$(document).ready();
+
 // Display current date
 const currentTime = moment().format("dddd MMMM Do YYYY, h:mm");
 
 // display text
 //get help with centering date
-$("#display-date").text(currentTime);
 
 // variable to be added to
 const empty = "";
 
 // loop to display times between 9am - 6pm
-for (var i = 9; i <= 18; i++) {
-  row = $(`<div class="row">`);
-  col1 = $(`<div class ="col-lg-2 hour">${displayAmorPm(i)}</div>`);
-  col2 = $(
-    `<div class ="col-lg-8 inputcontent"><input data-input="${i}" id="inputText${i}" class="form-control inputText" type="text" name="userInput"></div>`
-  );
-  col3 = $(
-    `<div class ="col-lg-2"><button data-id="${i}" id="savePlanner" class="btn btn-success btn-block"><i class="fas fa-save"></i> Save</button></div>`
-  );
-  row.append(col1);
-  row.append(col2);
-  row.append(col3);
-  $("#display-planner").append(row);
-  //get local storage
-}
+const renderTimeBlocks = () => {
+  for (let i = 9; i <= 18; i++) {
+    const row = $(`<div class="row">`);
+    const col1 = $(`<div class ="col-lg-2 hour">${displayAmorPm(i)}</div>`);
+    const col2 = $(
+      `<div class ="col-lg-8 inputcontent"><input data-input="${i}" id="inputText${i}" class="form-control inputText" type="text" name="userInput"></div>`
+    );
+    const col3 = $(
+      `<div class ="col-lg-2"><button data-id="${i}" id="savePlanner" class="btn btn-success btn-block"><i class="fas fa-save"></i> Save</button></div>`
+    );
+    row.append(col1);
+    row.append(col2);
+    row.append(col3);
+    $("#display-planner").append(row);
+    //get local storage
+  }
+  $("#display-planner").on("click", storeInput);
+};
 
 // create row elements to append
 
@@ -33,7 +35,7 @@ for (var i = 9; i <= 18; i++) {
 
 // Convert AM to PM
 function displayAmorPm(hour) {
-  var b = "";
+  let b = "";
   if (hour <= 12) {
     b = "AM";
   } else {
@@ -44,9 +46,26 @@ function displayAmorPm(hour) {
   return hour + " " + b;
 }
 
+const storeInput = (event) => {
+  console.log(event.target);
+  const target = $(event.target);
+  //   localStorage.setItem();
+  if (target.is("button")) {
+    //get user input then save to LS
+    // also save timeblock with LS to know which hour goes with which user input
+  }
+};
 // Function to get from LS
 
 // Update color - using a loop
-updateColor = () => {};
+const updateColor = () => {};
 // Interval to update color
-setInterval = () => {};
+const setInterval = () => {};
+
+const onReady = () => {
+  $("#display-date").text(currentTime);
+  renderTimeBlocks();
+  // read from local storage - render saved passwords
+};
+
+$(document).ready(onReady);
