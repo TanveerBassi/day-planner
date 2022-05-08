c6d88f96f4a7f8eb1f42334d5fc4157f86aea2ed
Tanveer Bassi <tanveer13a551@gmail.com>
2022-05-08 15:44:35 +0100

    JS comments written up to get with

--- assets/app.js
+++ assets/app.js
@@ -2,8 +2,18 @@
 
 // Display current date
 
-//Variable to append columns
+//Variable to append
 
 // loop to display times between 9am - 6pm
 
 // create row elements to append
+
+//create button and store click to LS
+
+// Convert AM to PM
+
+// Function to get from LS
+
+// Update color - using a loop
+
+// Interval to update color
