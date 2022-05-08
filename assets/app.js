f31932da870c9da895b142213e25e93ec8de4b58
Tanveer Bassi <tanveer13a551@gmail.com>
2022-05-08 14:46:36 +0100

    Updated CSS title and sub title

--- assets/app.js
+++ assets/app.js
@@ -1,1 +1,9 @@
-// ensure Jquery is ready
+// wait until Jquery is ready
+
+// Display current date
+
+//Variable to append columns
+
+// loop to display times between 9am - 6pm
+
+// create row elements to append
